1cf606438192d78917466ad91f564d958e3b6856
Hoagie Man <hoagieman5000@gmail.com>
2022-02-09 18:13:29 -0800

    Baisc functionality

--- src/components/favoriteslist/FavoritesCategory.tsx
+++ src/components/favoriteslist/FavoritesCategory.tsx
@@ -1,9 +1,9 @@
-import { Button, Divider } from "@mui/material";
-import React from "react";
+import { Autocomplete, Button, Divider, TextField } from "@mui/material";
+import React, { useState } from "react";
 import { StreamData, UserFollows } from "../../service/TwitchClientTypes";
 import { FlexCol, FlexRow } from "../../util/FlexBox";
 import { TwitchStream } from "../streamlist/TwitchStream";
-import { FavoriteCategory, FavoritesData } from "./FavoritesTypes";
+import { FavoriteCategory, FavoritesData, StreamFavoritesData } from "./FavoritesTypes";
 
 export interface FavoritesCategoryProps {
     favorites: FavoritesData
@@ -17,28 +17,53 @@ export interface FavoritesCategoryProps {
 export const FavoritesCategory = (props: FavoritesCategoryProps) => {
     const { category, streams, following, favorites } = props;
 
-    const favoritesInCategory = favorites.favorites.filter(favorite => favorite.categoryIds.has(category.id));
+    const [selectedStreamer, setSelectedStreamer] = useState<string | undefined | null>(undefined);
+
+    const favoritesInCategory = favorites.favorites.filter(favorite => favorite.categoryIds.includes(category.id));
     const liveStreamsInCategory = streams.filter(stream => !!favoritesInCategory.find(fav => fav.channelId === stream.user_id));
 
-    function onAddStreamer(channelId: string) {
-        let existingFavorite = favorites.favorites.find(fav => fav.channelId === channelId);
+    function onAddStreamer(streamer: UserFollows) {
+        let existingFavorite = favorites.favorites.find(fav => fav.channelId === streamer.to_id);
         if (!existingFavorite) {
-            // existingFavorite
-        }
-
-        if (existingFavorite && !existingFavorite?.categoryIds.has(category.id)) {
-            existingFavorite?.categoryIds.add(category.id);
-            props.onFavoritesChange(favorites)
+            existingFavorite = {
+                channelId: streamer.to_id,
+                channelName: streamer.to_name,
+                categoryIds: [category.id]
+            } as StreamFavoritesData;
+            favorites.favorites.push(existingFavorite);
+            props.onFavoritesChange({ ...favorites })
         } else {
-            console.log(`Channel ${channelId} is already is the ${category.label} category`)
+            if (existingFavorite && !existingFavorite?.categoryIds.includes(category.id)) {
+                existingFavorite?.categoryIds.push(category.id);
+                props.onFavoritesChange({ ...favorites })
+            } else {
+                console.log(`Channel ${streamer.to_id} is already is the ${category.label} category`)
+            }
         }
     }
 
-    return <FlexCol>
-        <FlexRow>
-            <div className="favorite-category">{category.label}</div>
-            <Button onClick={() => onAddStreamer("118391771")}>Add Streamer</Button>
-        </FlexRow>
+    return <FlexCol className="favorite-category-container">
+        <FlexCol>
+            <div className="favorite-category-name">{category.label}</div>
+            <FlexRow marginBottom={"10px"}>
+                <Autocomplete
+                    disablePortal
+                    id="followed-streamers-selector"
+                    size="small"
+                    options={following.map(f => f.to_name)}
+                    sx={{ width: 260 }}
+                    renderInput={(params) => <TextField {...params} label="Add Streamer" />}
+                    onChange={(event: any, newValue: string | null) => setSelectedStreamer(newValue)}
+                />
+                <Button onClick={() => {
+                    const streamer = following.find(f => f.to_name === selectedStreamer);
+                    if (streamer) {
+                        onAddStreamer(streamer)
+                    }
+                }}
+                >Add Streamer</Button>
+            </FlexRow>
+        </FlexCol>
         <FlexCol>
             {liveStreamsInCategory.map(liveStream => (
                 <TwitchStream
